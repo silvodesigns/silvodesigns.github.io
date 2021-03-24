1e2e9f9c4901b95ffe6b4bb675019b827d65ee05
Kevin Silvestre <kevsilvo@gmail.com>
2021-03-24 03:30:22 -0400

    initialized react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Kevin Silvestre
+         
+        </p>
+        <h1>WEB DEVELOPER</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
