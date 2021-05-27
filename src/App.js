d466bce01812504e0643efd0e73f880f562c184a
Kevin Silvestre <kevsilvo@gmail.com>
2021-05-27 15:36:28 -0400

    added intro component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header.js';
+import Intro from './components/Intro.js';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Intro></Intro>
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,24 @@
+import '../styles/intro.css';
+
+
+function Intro() {
+    return (
+        <div className="intro">
+            <div className="introduction">
+                <h1>Web Developer/
+            Designer,<br />
+                    Bringing<br />
+                        ideas to life<br />
+                            from Concept ,<br />
+                                Design & <br />
+                                    Development</h1>
+            </div>
+            <div className="description">
+                <p>Hello, i’m Kevin Silvestre, passionate about web developtment and design;
+                looking to be part of any creative & cross-functional team enviroment; Designer at heart, and coder by trade. </p>
+            </div>
+        </div >
+    );
+}
+
+export default Intro;
