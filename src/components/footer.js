d2a41c0057a881449387327f8ce5235be84f9c6d
Kevin Silvestre <kevsilvo@gmail.com>
2021-05-28 16:19:31 -0400

    built basic footer

---
+++ src/components/footer.js
@@ -0,0 +1,25 @@
+import '../styles/footer.css';
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <footer className="contact-info">
+                <div className="address">
+                    <p>Boston, MA</p>
+                </div>
+
+                <div className="connect">
+                    <p>LinkedIn</p>
+                    <p>Twitter</p>
+                </div>
+            </footer>
+            <div className="coppyrights">
+                <p>@2021 Kevin Silvestre. All Rights reserved. Legal info</p>
+
+            </div>
+
+        </div >
+    );
+}
+
+export default Footer;
