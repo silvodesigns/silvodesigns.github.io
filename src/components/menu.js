c6dff5d0f16bc9a8ed6ae862364618c1a113001d
Kevin Silvestre <kevsilvo@gmail.com>
2021-06-06 23:31:19 -0400

    menu items toogle menu on click

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,14 +4,21 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function Menu() {
 
+
+    const toggleMenu = event => {
+        const target = document.getElementById("nav-menu");
+        target.classList.toggle("open");
+    };
+
+
     return (
         <div className="nav-menu" id="nav-menu">
             <ul>
                 <BrowserRouter>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='work'>Work</Link></li>
-                    <li><Link to='resume'>Resume</Link></li>
-                    <li><Link to='contact'>Contact</Link></li>
+                    <li onClick={toggleMenu}> <Link to='/'>Home</Link></li>
+                    <li onClick={toggleMenu}> <Link to='work'>Work</Link></li>
+                    <li onClick={toggleMenu}> <Link to='resume'>Resume</Link></li>
+                    <li onClick={toggleMenu}> <Link to='contact'>Contact</Link></li>
 
 
                     {/* <Route exact path='/' component={ } />
