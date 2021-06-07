14310caf41697bb26808d203e21f4661e366d4e5
Kevin Silvestre <kevsilvo@gmail.com>
2021-06-06 23:48:34 -0400

    dynamically rendering components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+
 import Header from './components/Header.js';
 import Intro from './components/Intro.js';
 import Menu from './components/Menu.js';
@@ -9,10 +11,44 @@ import Footer from './components/Footer.js';
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Menu></Menu>
-      <Intro></Intro>
-      <Footer></Footer>
+      <Route exact path='/' render={props => {
+        return <div>
+          <Header></Header>
+          <Menu></Menu>
+          <Intro></Intro>
+          <Footer></Footer>
+        </div>
+
+      }} />
+
+      <Route path='/work' render={props => {
+        return <div>
+          <Header></Header>
+          <Menu></Menu>
+          <Footer></Footer>
+        </div>
+
+      }} />
+
+      <Route path='/resume' render={props => {
+        return <div>
+          <Header></Header>
+          <Menu></Menu>
+          <Footer></Footer>
+        </div>
+
+      }} />
+
+
+      <Route path='/contact' render={props => {
+        return <div>
+          <Header></Header>
+          <Menu></Menu>
+          <Footer></Footer>
+        </div>
+
+      }} />
+
 
     </div>
   );
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,5 @@
 import '../styles/menu.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function Menu() {
@@ -14,18 +14,15 @@ function Menu() {
     return (
         <div className="nav-menu" id="nav-menu">
             <ul>
-                <BrowserRouter>
-                    <li onClick={toggleMenu}> <Link to='/'>Home</Link></li>
-                    <li onClick={toggleMenu}> <Link to='work'>Work</Link></li>
-                    <li onClick={toggleMenu}> <Link to='resume'>Resume</Link></li>
-                    <li onClick={toggleMenu}> <Link to='contact'>Contact</Link></li>
+
+                <li onClick={toggleMenu}> <Link to='/'>Home</Link></li>
+                <li onClick={toggleMenu}> <Link to='work'>Work</Link></li>
+                <li onClick={toggleMenu}> <Link to='resume'>Resume</Link></li>
+                <li onClick={toggleMenu}> <Link to='contact'>Contact</Link></li>
 
 
-                    {/* <Route exact path='/' component={ } />
-                    <Route path='/work' component={ } />
-                    <Route path='/resume' component={ } />
-                    <Route path='contact' component={ } /> */}
-                </BrowserRouter>
+
+
             </ul>
         </div>
 
