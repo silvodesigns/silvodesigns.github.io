7282afc7eadbd0759b489aa9b39d805ab1f8f20f
Kevin Silvestre <kevsilvo@gmail.com>
2021-06-17 15:13:26 -0400

    implementing breakpoints

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,18 +5,23 @@ function Intro() {
     return (
         <div className="intro">
             <div className="introduction">
-                <h1>Web Developer/
-            Designer,<br />
-                    Bringing<br />
-                        ideas to life<br />
-                            from Concept ,<br />
-                                Design & <br />
-                                    Development</h1>
-            </div>
-            <div className="description">
+                <h1>Web Developer/ <br />
+                    Designer</h1>
+
+                <div>
+                    <p>
+                        Bringing ideas<br />
+                        to life from <br />
+                        concept , <br />
+                        design & <br />
+                        development
+                    </p>
+                </div>
+            </div >
+            {/* <div className="description">
                 <p>Hello, i’m Kevin Silvestre, passionate about web developtment and design;
-                looking to be part of any creative & cross-functional team enviroment; Designer at heart, and coder by trade. </p>
-            </div>
+                    looking to be part of any creative & cross-functional team enviroment; Designer at heart, and coder by trade. </p>
+            </div> */}
         </div >
     );
 }
