db2b324807a471668b7ac69fdf3e2a32c7caeacc
Kevin Silvestre <kevsilvo@gmail.com>
2023-01-01 02:20:26 -0500

    before switching back to updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,44 @@
-import '../styles/header.css';
-import logo from '../logo22.svg';
-import { Link } from 'react-router-dom';
+import "../styles/header.css";
+import logo from "../logo22.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const toggleMenu = (event) => {
+    const target = document.getElementById("nav-menu");
+    target.classList.toggle("open");
+  };
 
-    const toggleMenu = event => {
-        const target = document.getElementById("nav-menu");
-        target.classList.toggle("open");
-
-    };
-
-    return (
-        <div className="header">
-            <header className="App-header">
-                <div className="logo-container">
-                    <img src={logo} className="logo" alt="logo" />
-                    <p onClick={toggleMenu} class="menu-btm">menu</p>
-                </div>
-                <nav>
-                    <ul>
-                        <li id="menu-item"><div className="blue-icon"></div> <Link to='/'>home</Link></li>
-                        <li id="menu-item"><div className="blue-icon"></div> <Link to='work'>work</Link></li>
-                        <li id="menu-item"><div className="blue-icon"></div> <Link to='/'>experiences</Link></li>
-                        <li id="menu-item"><div className="blue-icon"></div> <Link to='/'>about</Link></li>
-                        <li id="menu-item"><div className="blue-icon"></div> <Link to='/'>resume</Link></li>
-                    </ul>
-                </nav>
-
-
-            </header>
-        </div >
-    );
+  return (
+    <div className="header">
+      <header className="App-header">
+        <div className="logo-container">
+          <img src={logo} className="logo" alt="logo" />
+          <p onClick={toggleMenu} class="menu-btm">
+            menu
+          </p>
+        </div>
+        <nav>
+          <ul>
+            <li id="menu-item">
+              <div className="blue-icon"></div> <Link to="/">home</Link>
+            </li>
+            <li id="menu-item">
+              <div className="blue-icon"></div> <Link to="work">work</Link>
+            </li>
+            <li id="menu-item">
+              <div className="blue-icon"></div> <Link to="/">experiences</Link>
+            </li>
+            <li id="menu-item">
+              <div className="blue-icon"></div> <Link to="/">about</Link>
+            </li>
+            <li id="menu-item">
+              <div className="blue-icon"></div> <Link to="/">resume</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
-
-
-
-
-
 
 export default Header;
