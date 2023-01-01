ba90052332de89ab025c25293ca0938d961a228d
Kevin Silvestre <kevsilvo@gmail.com>
2023-01-01 02:13:39 -0500

    Updates for Ministry

--- src/App.js
+++ src/App.js
@@ -1,59 +1,31 @@
-import './App.css';
-import { Route } from 'react-router-dom';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route } from "react-router-dom";
 
-import Header from './components/Header.js';
-import Intro from './components/Intro.js';
-import Intro2 from './components/intro2.js'
-import Menu from './components/Menu.js';
-import Work from './components/Work.js';
-import Footer from './components/Footer.js';
+import Header from "./components/Header.js";
+import Intro from "./components/Intro.js";
+import Footer from "./components/Footer.js";
 
-
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' render={props => {
-        return <div>
-          <Header></Header>
-          <Menu></Menu>
-          <Intro></Intro>
-          <Intro2></Intro2>
-          <Footer></Footer>
-        </div>
-
-      }} />
-
-      <Route path='/work' render={props => {
-        return <div>
-          <Header></Header>
-          <Menu></Menu>
-          <Work></Work>
-          <Footer></Footer>
-        </div>
-
-      }} />
-
-      <Route path='/resume' render={props => {
-        return <div>
-          <Header></Header>
-          <Menu></Menu>
-          <Footer></Footer>
-        </div>
-
-      }} />
-
-
-      <Route path='/contact' render={props => {
-        return <div>
-          <Header></Header>
-          <Menu></Menu>
-          <Footer></Footer>
-        </div>
-
-      }} />
-
-
+      <Container>
+        <Route
+          exact
+          path="/"
+          render={(props) => {
+            return (
+              <div>
+                <Header></Header>
+                <Intro></Intro>
+                <Footer></Footer>
+              </div>
+            );
+          }}
+        />
+      </Container>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,44 @@
-import '../styles/footer.css';
+import "../styles/footer.css";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Inst from "../instgram_icon.png";
+import You from "../yt_icon.png";
+import Fb from "../fb_icon.png";
+import Tik from "../tik.png";
 
 function Footer() {
-    return (
-        <div className="footer-container">
-            <footer className="contact-info">
-                <div className="address">
-                    <p>Boston, MA</p>
-                </div>
-
-                <div className="connect">
-                    <p><a href="https://www.linkedin.com/in/kevinsilvestre/">LinkedIn</a></p>
-                    <p><a href="https://github.com/silvodesigns">GitHub</a></p>
-                </div>
-            </footer>
-            <div className="coppyrights">
-                <p>@2021 Kevin Silvestre. All Rights reserved. Legal info</p>
-            </div>
-
-        </div >
-    );
+  return (
+    <Container>
+      <Row style={{ paddingTop: "30px", paddingBottom: "40px" }}>
+        <Col className="text-center">
+          {" "}
+          <a href="https://www.instagram.com/kevinjsilvestre/">
+            <img src={Inst} className="social_icons" alt="instagram" />
+          </a>
+        </Col>
+        <Col className="text-center">
+          {" "}
+          <a href="https://www.youtube.com/@KevinSilvestre/">
+            <img src={You} className="social_icons" alt="YouTube" />
+          </a>
+        </Col>
+        <Col className="text-center">
+          {" "}
+          <a href="https://www.facebook.com/kevinjosesilvestre/">
+            <img src={Fb} className="social_icons" alt="FaceBook" />
+          </a>
+        </Col>
+        <Col className="text-center">
+          <a href="https://www.tiktok.com/@kevinjsilvestre?_t=8XxsXSrSsH5&_r=1">
+            <img src={Tik} className="social_icons" alt="TikTok" />
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer;
